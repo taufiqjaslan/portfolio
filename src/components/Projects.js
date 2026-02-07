9e996527d809b1d8239f2fe2b43d285c71c0b73a
Muhammad Taufiq Bin Mohd Jaslan <105698083+taufiqjaslan@users.noreply.github.com>
2026-02-07 15:20:36 +0800

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -57,7 +57,7 @@ export const Projects = () => {
                 <h2>Portfolio & Experience</h2>
                 <p>Software Developer with over 2 years of experience delivering high-performance Laravel applications, RESTful APIs, and scalable cloud solutions.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center flex-column flex-sm-row" id="pills-tab">
                     <Nav.Item>
                       <Nav.Link eventKey="first">Major Projects</Nav.Link>
                     </Nav.Item>
@@ -106,7 +106,7 @@ export const Projects = () => {
                           <div className="roadmap-content">
                             <span className="roadmap-date">Feb 2024 - Aug 2024</span>
                             <h3>Junior Software Developer (Full Time)</h3>
-                            <h4>Huacomm Telecommunications Engineering</h4>
+                            <h4>Huacomm Telecommunications Engineering (M) Sdn. Bhd.</h4>
                             <ul>
                               <li>Directed the development of an accounting module, delivering 90% of the system a month early.</li>
                               <li>Implemented high-performance RESTful APIs, increasing data retrieval speed by 30%.</li>
@@ -120,7 +120,7 @@ export const Projects = () => {
                           <div className="roadmap-dot"></div>
                           <div className="roadmap-content">
                             <span className="roadmap-date">Mar 2024 - Sept 2024</span>
-                            <h3>Software Developer (Freelance)</h3>
+                            <h3>Pixel Dynamic Technology (Freelance)</h3>
                             <h4>Self-Employed - Remote</h4>
                             <ul>
                               <li>Developed a responsive Laravel/Livewire system, improving data organization by 80%.</li>
@@ -135,11 +135,26 @@ export const Projects = () => {
                           <div className="roadmap-content">
                             <span className="roadmap-date">Aug 2023 - Jan 2024</span>
                             <h3>Software Developer (Internship)</h3>
-                            <h4>Huacomm Telecommunications Engineering</h4>
+                            <h4>Huacomm Telecommunications Engineering (M) Sdn. Bhd.</h4>
                             <ul>
                               <li>Maintained Laravel/Livewire apps, improving interface and functionality by 80%.</li>
                               <li>Collaborated with cross-functional teams for accounting software solutions.</li>
                               <li>Debugged production issues, providing 100% permanent solutions.</li>
+                            </ul>
+                          </div>
+                        </div>
+
+                        {/* Internship */}
+                        <div className="roadmap-item">
+                          <div className="roadmap-dot"></div>
+                          <div className="roadmap-content">
+                            <span className="roadmap-date">Sep 2020 - Feb 2021</span>
+                            <h3>PHP Developer (Internship)</h3>
+                            <h4>Tombark Technology Sdn. Bhd.</h4>
+                            <ul>
+                              <li>Developed a responsive internal HR system using PHP, improving data organization by 80%.</li>
+                              <li>Collaborated with cross-functional teams to deliver high-quality software solutions and resolve critical system bugs.</li>
+                              <li>Produced User Acceptance Testing (UAT) documentation and step-by-step user manuals, ensuring high software quality and user readiness.</li>
                             </ul>
                           </div>
                         </div>
