594207b7822b0089351c49d110cf5f2c55b1813a
Muhammad Taufiq Bin Mohd Jaslan <105698083+taufiqjaslan@users.noreply.github.com>
2026-02-07 15:25:27 +0800

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -45,7 +45,7 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>MySQL Optimization</h5>
+                                <h5>MySQL</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
